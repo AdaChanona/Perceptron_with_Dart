520a0f36cbd8016bc84b705c15072682b94be380
AdaChanona <ada.gamboa.chanona2510@gmail.com>
2022-07-09 15:42:15 -0500

    First Commit

---
+++ bin/perceptron_with_dart.dart
@@ -0,0 +1,5 @@
+import 'package:perceptron_with_dart/perceptron_with_dart.dart' as perceptron_with_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${perceptron_with_dart.calculate()}!');
+}
